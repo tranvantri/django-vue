2b12a5ed4b0c121655e0054eb81dc33f21f44b4d
Tri Tran Van <tritranv@nashtechglobal.com>
2022-07-11 15:13:07 +0700

    fix secret key

--- django_vue/django_vue/settings.py
+++ django_vue/django_vue/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tuh32b4t3(a5xqp1%ufqaaxevnqugpq0u3gah*nu6ig_scsn1f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
